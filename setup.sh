609a8de7576e71f7e619f7cc505865417a6f267a
Shuma Shinozaki <shu.shinozaki@gmail.com>
2018-05-08 23:24:15 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ dotfiles=(.gitconfig .gitignore_global .tmux.conf .vimrc .zshrc)
 
 for file in ${dotfiles[@]}
 do
-    ln -s ~/src/github.com/syuma1214/dotfiles/$file $HOME/$file
+    ln -s ~/src/github.com/shinoshu/dotfiles/$file $HOME/$file
 done
