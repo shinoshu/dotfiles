f730116d44da55718b8438b6d6d8439acfd53a12
Shuma Shinozaki <shu.shinozaki@gmail.com>
2018-11-09 19:11:53 +0900

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+brew install bat
+brew install exa
+brew install ghq
+brew install gibo
+brew install hub
+brew install jq
+brew install node
+brew install nodebrew
+brew install peco
+brew install postgresql
+brew install pyenv
+brew install rbenv
+brew install tig
+brew install zsh
