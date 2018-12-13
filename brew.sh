1cff24233a0acf7a841653ee31ce8408d47cd756
Shuma Shinozaki <shu.shinozaki@gmail.com>
2018-12-13 14:16:23 +0900

    update .zshrc

--- brew.sh
+++ brew.sh
@@ -17,8 +17,11 @@ brew install tig
 brew install zsh
 
 brew install caskroom/cask/brew-cask
+brew cask install alfred
 brew cask install docker
 brew cask install google-chrome
 brew cask install google-japanese-ime
 brew cask install slack
 brew cask install visual-studio-code
+
+brew cask alfred link
