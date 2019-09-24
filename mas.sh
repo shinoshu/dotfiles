c88b603c1dbd647c7d06f43c86c7b8b16f9bce7d
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-09-25 00:13:28 +0900

    add mas.sh

---
+++ mas.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -x
+
+mas install 417375580 # BetterSnapTool
+mas install 587512244 # Kaleidoscope
+mas install 406056744 # Keynote
+mas install 405399194 # Kindle
+mas install 539883307 # LINE
+mas install 568494494 # Pocket
