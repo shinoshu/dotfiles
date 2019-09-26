4b901e51bd44b31e7b4f772bb282773e30d14732
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-09-26 15:13:06 +0900

    update

--- mas.sh
+++ mas.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -x
 
-mas install 417375580 # BetterSnapTool
-mas install 587512244 # Kaleidoscope
-mas install 406056744 # Keynote
-mas install 405399194 # Kindle
-mas install 539883307 # LINE
-mas install 568494494 # Pocket
+mas install 417375580  # BetterSnapTool
+mas install 406056744  # Evernote
+mas install 587512244  # Kaleidoscope
+mas install 405399194  # Kindle
+mas install 539883307  # LINE
+mas install 425955336  # Skitch
+mas install 957734279  # TogglDesktop
+mas install 1278508951 # Trello
+mas install 457622435  # Yoink
+mas install 568494494  # Pocket
+mas install 497799835  # Xcode
