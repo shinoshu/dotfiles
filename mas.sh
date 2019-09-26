5f47673d45c36cc69ec3c70140bee8722a0cd7fc
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-09-26 15:27:17 +0900

    update

--- mas.sh
+++ mas.sh
@@ -5,6 +5,7 @@ mas install 406056744  # Evernote
 mas install 587512244  # Kaleidoscope
 mas install 405399194  # Kindle
 mas install 539883307  # LINE
+mas install 1274495053 # Microsoft To Do
 mas install 425955336  # Skitch
 mas install 957734279  # TogglDesktop
 mas install 1278508951 # Trello
